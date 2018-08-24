cd4e2463afdd992844bda4e6b3886efce3bcc60d
Michael Law <michaellaw@gmx.com>
2018-08-24 12:40:13 -0400

    day 2

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit]
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      p "article saved!"
+      redirect_to @article
+    else
+      p 'Article rejected for errors'
+      p @article.errors.messages
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+    @articles = Article.all.order('CREATED_AT DESC')
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :author, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'pages/home'
+
+  resources :articles
+
+  get '/write' => 'articles#new'
+  get 'articles/edit'
+  get 'articles/show'
+  get 'articles/index'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
