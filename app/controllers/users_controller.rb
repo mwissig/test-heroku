30854ac11e358bef3bcd5d22c133877f7fe80978
Michael Law <michaellaw@gmx.com>
2018-08-27 11:58:34 -0400

    added passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_one_attached :photo
   has_many :articles
-  
+  has_secure_password
+  validates :password, presence: true
+  validates :email, presence: true, length: { maximum: 100 }
   def full_name
     self.first_name + " " + self.last_name
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
 end
