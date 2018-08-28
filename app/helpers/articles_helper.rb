68586a8222646de264020aff454c39574c63d99f
Michael Law <michaellaw@gmx.com>
2018-08-28 10:11:33 -0400

    ready for forms

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+
+  def long_article?(a)
+    if a.content.split('').count > 100
+      msg = 'You may want to read this later'
+    else
+      msg = 'This is a quick read'
+    end
+    return msg
+  end
+
 end
